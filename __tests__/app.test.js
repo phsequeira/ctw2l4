e724945d78ed3c05b25d7386491c4adcec59b139
phsequeira <phsequeira@yahoo.com>
2021-04-01 11:03:03 -0700

    fine tuning

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,15 @@
 require('dotenv').config();
 
-
-
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
 
-
 describe('app routes', () => {
-  
-
   test('returns the current weather in salem or', async() => {
-
     const expectation =
       {
         'forecast': 'Clear sky',
-        'tempature': 66.4,
-        'time': 'Wed Mar 31 2021'
+        'tempature': 53.8,
+        'time': 'Thu Apr 01 2021',
       };
     
     const data = await fakeRequest(app)
@@ -25,49 +19,48 @@ describe('app routes', () => {
 
     expect(data.body[0]).toEqual(expectation);
   });
-
-  test('returns the weather in salem or for the next 16 days', async() => {
+  test('returns the weather in salem or for the next 7 days', async() => {
 
     const expectation = [
       {
-        'forecast': 'Few clouds',
-        'tempature': 11.1,
-        'time': 'Wed Mar 31 2021'
-      },
-      {
-        'forecast': 'Broken clouds',
-        'tempature': 10.5,
+        'forecast': 'Scattered clouds',
+        'tempature': 51.4,
         'time': 'Thu Apr 01 2021'
       },
       {
         'forecast': 'Broken clouds',
-        'tempature': 9,
+        'tempature': 47.5,
         'time': 'Fri Apr 02 2021'
       },
       {
         'forecast': 'Overcast clouds',
-        'tempature': 9.2,
+        'tempature': 49.2,
         'time': 'Sat Apr 03 2021'
       },
       {
-        'forecast': 'Light shower rain',
-        'tempature': 7.9,
+        'forecast': 'Moderate rain',
+        'tempature': 41,
         'time': 'Sun Apr 04 2021'
       },
       {
         'forecast': 'Overcast clouds',
-        'tempature': 7.5,
+        'tempature': 40.3,
         'time': 'Mon Apr 05 2021'
       },
       {
         'forecast': 'Overcast clouds',
-        'tempature': 6.8,
+        'tempature': 41.6,
         'time': 'Tue Apr 06 2021'
+      },
+      {
+        'forecast': 'Overcast clouds',
+        'tempature': 43.6,
+        'time': 'Wed Apr 07 2021'
       }
     ];
     
     const data = await fakeRequest(app)
-      .get('/weather/16Days?city=salem&state=or')
+      .get('/weather/7Days?city=salem&state=or')
       .expect('Content-Type', /json/)
       .expect(200);
 
